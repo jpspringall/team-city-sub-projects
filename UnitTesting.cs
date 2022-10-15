26317c55d187166767193964dc845a160280b262
James Springall <james_springall@hotmail.com>
2022-10-15 11:12:46 +1100

    no message

--- UnitTesting.cs
+++ UnitTesting.cs
@@ -24,5 +24,15 @@ namespace TCSonarCube
             42.Should().Be(42);
             42.Should().Be(42);
         }
+
+        [Fact]
+        public void Bug()
+        {
+            int target = -5;
+            int num = 3;
+
+            target = -num;  // Noncompliant; target = -3. Is that really what's meant?
+            target = +num; // Noncompliant; target = 3
+        }
     }
 }
