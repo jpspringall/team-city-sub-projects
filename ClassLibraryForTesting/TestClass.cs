b1b89b2aca1699a8658ca7edc7ad09bdad94f5d1
James Springall <james_springall@hotmail.com>
2022-10-15 11:39:20 +1100

    no message

---
+++ ClassLibraryForTesting/TestClass.cs
@@ -0,0 +1,30 @@
+﻿namespace ClassLibraryForTesting
+{
+    public class TestClass
+    {
+        public void BuggyCode()
+        {
+            int target = -5;
+            int num = 3;
+
+            target = -num;  // Noncompliant; target = -3. Is that really what's meant?
+            target = +num; // Noncompliant; target = 3
+        }
+
+        public void DuplicateCode()
+        {
+            var x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;// Noncompliant; target = 3
+        }
+
+
+    }
+}
