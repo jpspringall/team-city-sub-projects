133974dd5fe347d51652ee7fba516b9ff03b0a6e
James Springall <james_springall@hotmail.com>
2022-10-15 13:56:11 +1100

    no message

--- ClassLibraryForTesting/TestClass.cs
+++ ClassLibraryForTesting/TestClass.cs
@@ -83,6 +83,87 @@
 
         }
 
+        public void BuggyCodeBranch()
+        {
+            int target = -5;
+            int num = 3;
+
+            target = -num;
+            target = +num;
+
+            target = -num;
+            target = +num;
+
+            target = -num;
+            target = +num;
+
+            target = -num;
+            target = +num;
+
+            target = -num;
+            target = +num;
+        }
+
+        public void DuplicateCodeBranch()
+        {
+            var x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+            x = 42;
+
+        }
+
 
     }
 }
