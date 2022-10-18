18668f70c98a9fe647930f294b6f64a910564758
jpspringall <jpspringall@TeamCity>
2022-10-18 10:31:47 +0000

    TeamCity change in 'Team City Sonar Cube' project: 'Build' build configuration settings were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -148,6 +148,7 @@ object Build : BuildType({
             }
         }
         pullRequests {
+            enabled = false
             vcsRootExtId = "${DslContext.settingsRoot.id}"
             provider = github {
                 authType = vcsRoot()
