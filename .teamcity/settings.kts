e8139c673ce7530036f58f77777dd7ff7a8bc521
James Springall <james_springall@hotmail.com>
2022-10-20 15:29:12 +1100

    Param Per Build Configuration

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -37,9 +37,6 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2022.04"
 
 project {
-params {
-    param("teamcity.pullRequest.number", "master")
-}
     vcsRoot(HttpsGithubComJpspringallTeamCitySonarCubeRefsHeadsMaster1)
     buildType(Build)
     buildType(PullRequestBuild)
