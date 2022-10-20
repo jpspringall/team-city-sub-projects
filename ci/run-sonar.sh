10824a5a6dde5ec2b21c1f502a2c87c2944de032
jpspringall <james_springall@hotmail.com>
2022-10-20 18:56:00 +1100

    testing script (#45)
    * testing script
    
    * Testing Script
    
    * fix script
    
    * No project folder
    
    * fix script

--- ci/run-sonar.sh
+++ ci/run-sonar.sh
@@ -3,13 +3,14 @@ set -e
 
 echo "Running Script"
 
-while getopts :s:u:p:n: flag
+while getopts :s:u:p:n:v: flag
 do
     case "${flag}" in
         s) server=${OPTARG};; 
         u) user=${OPTARG};; 
         p) password=${OPTARG};; 
         n) number=${OPTARG};;
+        v) version=${OPTARG};;
         \?) echo "Invalid option: -$OPTARG" >&2;; 
     esac
 done
@@ -19,3 +20,19 @@ echo "User $user";
 echo "Password $password"
 echo "Number $number"
 
+#Not needed for now
+#cd project
+#/v:"%build.vcs.number%" \
+
+dotnet-sonarscanner begin \
+    /k:"SonarCubeTest" \
+    /n:"SonarCubeTest" \
+    /d:sonar.host.url="$server" \
+    /d:sonar.login="$user" \
+    /d:sonar.password="$password" \
+    /d:sonar.pullrequest.key="$number" \
+    /d:sonar.pullrequest.branch="pull/$number" \
+    /d:sonar.pullrequest.base="master" \
+    /d:sonar.cs.opencover.reportsPaths="**/coverage.opencover.xml"
+dotnet test -v n TCSonarCube.sln --filter 'FullyQualifiedName~Test.Unit' -p:CollectCoverage=true -p:CoverletOutputFormat=opencover%2cteamcity --results-directory "testresults"
+dotnet-sonarscanner end /d:sonar.login="$user" /d:sonar.password="$password"
