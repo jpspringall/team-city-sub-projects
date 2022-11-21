fb0dfff470036cc3b1c380a9302fecab333475b6
James Springall <james_springall@hotmail.com>
2022-11-21 20:42:07 +1100

    Second test

---
+++ project/TSonarCube2/UnitTesting.cs
@@ -0,0 +1,31 @@
+using FluentAssertions;
+using Xunit;
+
+namespace TSonarCube2
+{
+    public class UnitTesting
+    {
+        [Fact]
+        public void Pass()
+        {
+            42.Should().Be(42);
+        }
+
+        [Fact]
+        public void Fail()
+        {
+            42.Should().Be(42);
+
+        }
+
+        [Fact]
+        public void Bug()
+        {
+            int target = -5;
+            int num = 3;
+
+            target = -num;  // Noncompliant; target = -3. Is that really what's meant?
+            target = +num; // Noncompliant; target = 3
+        }
+    }
+}
