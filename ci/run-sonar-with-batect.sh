70100d6175edb1719dabe6bd84cafcc2075f5950
James Springall <james_springall@hotmail.com>
2022-12-28 18:40:18 +1100

    set -e

--- ci/run-sonar-with-batect.sh
+++ ci/run-sonar-with-batect.sh
@@ -61,6 +61,7 @@ fi
 set +e
 dotnet test TCSonarCube.sln -c Release -p:CollectCoverage=true -p:CoverletOutputFormat=opencover%2cteamcity --results-directory "/test-results" --logger 'trx;logfilename=testresults.trx' || error=true
 
+set -e
 if [ "$sonarUse" -eq 1 ]; then
     dotnet-sonarscanner end /d:sonar.login="$user" /d:sonar.password="$password"
 fi
